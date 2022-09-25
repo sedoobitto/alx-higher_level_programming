9e3bf2e223caddc48f2716b1bc65f14a4f3e9a3a
Sedoo Bitto <sedoobitto@gmail.com>
2022-09-25 12:23:33 +0000

    Finds biggest integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
-
 def max_integer(my_list=[]):
-    if my_list:
-        max = my_list[0]
-        for i in my_list:
-            if i > max:
-                max = i
-        return max
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Statu
+    if len(my_list) == 0:
+        return None
+
+    max_n = min(my_list)
+    for i in my_list:
+        if i > max_n:
+            max_n = i
+
+    return max_n
