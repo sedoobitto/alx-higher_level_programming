ad7eb2b74741049883e57be31bbfb981c6ff4741
Sedoo Bitto <sedoobitto@gmail.com>
2022-09-28 17:31:35 +0000

    Returns the numbers of keys in a dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+# 5-number_keys.py
+# Sedootershima Bitto <sedoobitto@gmail.com>
 
 def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
     return (len(a_dictionary))
-
