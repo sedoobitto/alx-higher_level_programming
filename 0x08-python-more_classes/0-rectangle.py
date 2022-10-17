6325b4984b11f5b303829a57049911a94503a8da
Sedoo Bitto <sedoobitto@gmail.com>
2022-10-17 05:34:02 +0000

    Defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    0-rectangle module
+    Empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+        Empty class Rectangle that defines a rectangle
+    """
+    pass
